72f5f074bcb51400132c4cd66017122b42b6b240
Tanush Mathur <devspark2501@gmail.com>
2026-08-11 23:28:45 +0530

    Code deployed for CartPage,

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,15 +1,39 @@
+
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+const CartPage({super.key});
 
-  @override
+@override
   State<CartPage> createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Cart Page"),);
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            Text(
+            "Your Cart Items",
+            style: TextStyle(fontSize: 20),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("Cart Item ${index + 1}"),);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
